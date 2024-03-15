99e224817f50056f20efd4fecb856df1a80edb3d
Courecta <41247044s@ntnu.edu.tw>
2024-03-16 02:44:38 +0800

    some changes

--- cpp/basic-Calculator.h
+++ cpp/basic-Calculator.h
@@ -5,3 +5,8 @@
 
 using namespace std;
 
+// Function prototypes
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
--- cpp/basic-calculator.cpp
+++ cpp/basic-calculator.cpp
@@ -1,8 +1,32 @@
 #include "basic-calculator.h"
 
+int add(int a, int b)
+{
+    return a + b;
+}
+
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
+int divide(int a, int b)
+{
+    return a / b;
+}
+
 int main()
 {
-    
-    
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << add(a, b) << endl;                                                                                                                                                                                                                                                    
     return 0;
 }
